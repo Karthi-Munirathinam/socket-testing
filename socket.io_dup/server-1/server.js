6f3d2a7f27a15750aecc7b4f34b2df9a6558393e
karthi <karthi@posbytz.com>
2022-01-20 17:14:12 +0530

    bug fix

--- socket.io_dup/server-1/server.js
+++ socket.io_dup/server-1/server.js
@@ -9,10 +9,8 @@ const io = require('socket.io')(server,{
 
 app.get('/message',(req, res, next) => {
     io.on('connect', socket => {
-        socket.on('connect-2222',(data,id) => {
-            console.log(data,id)
-        })
-        socket.emit('configure-1234','delhi')
+        console.log('hello from socket')
+        socket.broadcast.emit('configure-1234','delhi')
     })
     res.send('success')
 })
