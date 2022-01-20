d85b608bd54a54b532a34633262dcae94727e29b
karthi <karthi@posbytz.com>
2022-01-20 16:48:08 +0530

    bug fix

--- socket.io_dup/server-1/server.js
+++ socket.io_dup/server-1/server.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
-const io = require('socket.io')(process.env.PORT);
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
-io.on('connection', socket => {
-    socket.on('connect-2222',(data,id) => {
-        console.log(data,id)
+app.get('/message',(req, res, next) => {
+    io.on('connection', socket => {
+        socket.on('connect-2222',(data,id) => {
+            console.log(data,id)
+        })
+        socket.emit('configure-1234','delhi')
     })
-    socket.emit('configure-1234','delhi')
+    res.send('success')
 })
 
-
+app.listen(process.env.PORT || 3000, () => console.log(`app is listening in port:${process.env.PORT}`))
