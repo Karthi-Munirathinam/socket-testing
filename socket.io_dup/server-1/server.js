4b2ced526e02997dcb97b475d7054ce5b082fa24
karthi <karthi@posbytz.com>
2022-01-20 17:17:55 +0530

    bug fix

--- socket.io_dup/server-1/server.js
+++ socket.io_dup/server-1/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const http = require('http');
-const server = http.createServer(app);
+const server = http.Server(app);
 const io = require('socket.io')(server,{
     path:'/message'
 });
